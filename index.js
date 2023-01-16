86bbadf878c304d70f4149c7d0aa00afb8a1a7d7
Konrad <k.korzeniewski94@gmail.com>
2023-01-16 03:02:21 +0100

    Create all game logic

--- index.js
+++ index.js
@@ -21,8 +21,61 @@ const gameboard = (function () {
 		[2, 4, 6],
 	];
 
-	const render = function () {
-		elements.gameboard.innerText = "";
+	document.querySelector("dialog").addEventListener("click", function (e) {
+		if (this !== e.target) return;
+		this.close();
+	});
+
+	document.querySelector(".resetBtn").addEventListener("click", () => {
+		game.resetGame();
+	});
+
+	const resetGameboard = () => {
+		result.forEach((el, index) => (result[index] = " "));
+		render();
+	};
+
+	const checkWinStructure = () => {
+		let winningStructure = null;
+		winningCombinations.some((arr) => {
+			const sign1 = result[arr[0]];
+			const sign2 = result[arr[1]];
+			const sign3 = result[arr[2]];
+			if (
+				sign1 === sign2 &&
+				sign2 === sign3 &&
+				sign1 === sign3 &&
+				sign1 !== " " &&
+				sign2 !== " " &&
+				sign3 !== " "
+			) {
+				winningStructure = arr;
+				return true;
+			} else false;
+		});
+		return winningStructure;
+	};
+
+	const isChanceToWin = () => {
+		return winningCombinations.some((arr) => {
+			const sign1 = result[arr[0]];
+			const sign2 = result[arr[1]];
+			const sign3 = result[arr[2]];
+
+			if (sign1 === sign2 && sign3 === " ") return true;
+			else if (sign2 === sign3 && sign1 === " ") return true;
+			else if (sign1 === sign3 && sign2 === " ") return true;
+			else if (
+				(sign1 === " " && sign2 === " ") ||
+				(sign2 === " " && sign3 === " ") ||
+				(sign1 === " " && sign3 === " ")
+			)
+				return true;
+			else return false;
+		});
+	};
+
+	const createStructure = () => {
 		result.forEach((el, index) => {
 			const htmlElement = document.createElement("div");
 			htmlElement.setAttribute("data-number", index);
@@ -33,11 +86,46 @@ const gameboard = (function () {
 
 			document.getElementById("gameboard").appendChild(htmlElement);
 		});
-
-		// checkStructure()
 	};
 
-	return { render, result };
+	const render = function () {
+		elements.gameboard.innerText = "";
+		createStructure();
+
+		const winningPositions = checkWinStructure();
+
+		if (winningPositions === null && isChanceToWin() === false) {
+			document.getElementById("winner").innerText = `Tie`;
+			document.querySelector("dialog").showModal();
+			document.querySelector(".resetBtn>button").innerHTML = "New Game";
+		} else if (winningPositions?.length === 3) {
+			const tile1 = document.querySelector(
+				`[data-number='${winningPositions[0]}']`,
+			);
+			const tile2 = document.querySelector(
+				`[data-number='${winningPositions[1]}']`,
+			);
+			const tile3 = document.querySelector(
+				`[data-number='${winningPositions[2]}']`,
+			);
+
+			document
+				.querySelectorAll(".box")
+				.forEach((el) => el.classList.add("not-click"));
+
+			tile1.classList.add("win");
+			tile2.classList.add("win");
+			tile3.classList.add("win");
+
+			const winnerName = game.showCurrentPlayer().name;
+
+			document.getElementById("winner").innerText = `${winnerName} won`;
+			document.querySelector("dialog").showModal();
+			document.querySelector(".resetBtn>button").innerHTML = "New Game";
+		}
+	};
+
+	return { render, result, resetGameboard };
 })();
 
 const player = function (playerName) {
@@ -48,7 +136,9 @@ const player = function (playerName) {
 const game = (function () {
 	const players = [];
 	let currentPlayerMove = null;
+	const showCurrentPlayer = () => currentPlayerMove;
 	const setPlayer = (name) => players.push(player(name));
+	const getPlayers = () => players;
 
 	function setCurrentPlayerMove(player) {
 		currentPlayerMove = player;
@@ -61,12 +151,27 @@ const game = (function () {
 	}
 
 	const createPlayersNames = function () {
-		players.forEach((player) => {
+		getPlayers().forEach((player) => {
 			const p = document.createElement("p");
 			p.setAttribute("data-player", player.name);
 			p.innerText = player.name;
 			elements.namesBox.appendChild(p);
 		});
+	};
+
+	const disapperNameForm = () => {
+		const form = document.getElementById("getPlayersNames");
+		form.style.animationPlayState = "running";
+		form.addEventListener("animationend", function () {
+			this.style.display = "none";
+			showGameBoard();
+		});
+	};
+
+	const showGameBoard = () => {
+		elements.gameboard.style.animationPlayState = "running";
+		elements.gameboard.style.display = "grid";
+		elements.namesBox.style.animationPlayState = "running";
 	};
 
 	const formNameAnimation = () => {
@@ -75,6 +180,9 @@ const game = (function () {
 
 	const markPosition = (e) => {
 		if (gameboard.result[e.target.dataset.number] !== " ") return;
+
+		document.querySelector(".resetBtn").style.display = "block";
+		document.querySelector(".resetBtn").style.animationPlayState = "running";
 
 		gameboard.result[e.target.dataset.number] = currentPlayerMove.sign;
 		gameboard.render();
@@ -86,21 +194,48 @@ const game = (function () {
 	const startGame = () => {
 		elements.nameInput.value = "";
 		if (players.length < 2) return;
-		document.getElementById("getPlayersNames").style.display = "none";
+
 		players[0].sign = "X";
 		players[1].sign = "O";
+
 		createPlayersNames();
 		setCurrentPlayerMove(players[0]);
+		disapperNameForm();
+
 		gameboard.render();
 	};
-	return { formNameAnimation, markPosition, setPlayer, startGame, players };
+
+	const resetGame = () => {
+		setCurrentPlayerMove(players[0]);
+		document.querySelector(".resetBtn>button").innerHTML = "Reset";
+		document.querySelector(".resetBtn").style.display = "none";
+		gameboard.resetGameboard();
+	};
+
+	return {
+		formNameAnimation,
+		markPosition,
+		resetGame,
+		setPlayer,
+		startGame,
+		getPlayers,
+		showCurrentPlayer,
+	};
 })();
 
 elements.submitBtn.addEventListener("click", (e) => {
 	e.preventDefault();
-	if (elements.nameInput === "" && elements.nameInput.length <= 3) return;
+
+	if (
+		elements.nameInput.value.trim() === "" ||
+		elements.nameInput.value.trim().length < 3
+	)
+		return;
+
 	game.setPlayer(elements.nameInput.value);
-	if (game.players.length === 1) game.formNameAnimation();
+
+	if (game.getPlayers().length === 1) game.formNameAnimation();
+
 	game.startGame();
 });
 
